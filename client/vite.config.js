8471efe99f66418153857ff507f3e7e646cd7cf2
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-07-10 10:39:38 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import eslint from "vite-plugin-eslint";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
@@ -24,21 +20,3 @@ export default defineConfig({
     },
   },
 });
-// // vite.config.js
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import eslint from "vite-plugin-eslint";
-
-// export default defineConfig({
-//   plugins: [react(), eslint()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://cropify-deploy-server.vercel.app/",
-//         changeOrigin: true,
-//         secure: true,
-//         rewrite: (path) => path.replace(/^/api/, ''),
-//       },
-//     },
-//   },
-// });
