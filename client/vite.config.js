c570649bbdc7e7b5658641d7696b50625ee62214
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-07-10 09:22:58 +0530

    frontend

---
+++ client/vite.config.js
@@ -0,0 +1,39 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://cropify-server.vercel.app/",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+// // vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig({
+  plugins: [react(), eslint()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://cropify-server.vercel.app/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@emotion/react": "@emotion/react",
+      "@emotion/styled": "@emotion/styled",
+    },
+  },
+});
