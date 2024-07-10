c4a7fb82a64a1ade95c5539c72af5662bc692168
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-07-10 10:04:44 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,6 @@
 // import react from "@vitejs/plugin-react";
 // import eslint from "vite-plugin-eslint";
 
-// export default defineConfig({
-//   plugins: [react(), eslint()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://cropify-server.vercel.app/",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// });
-// // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
@@ -24,11 +11,34 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://cropify-deploy-server.vercel.app/",
+        target: "https://cropify-server.vercel.app",
         changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^/api/, ''),
+        secure: false,
       },
     },
   },
+  resolve: {
+    alias: {
+      "@emotion/react": "@emotion/react",
+      "@emotion/styled": "@emotion/styled",
+    },
+  },
 });
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://cropify-deploy-server.vercel.app/",
+//         changeOrigin: true,
+//         secure: true,
+//         rewrite: (path) => path.replace(/^/api/, ''),
+//       },
+//     },
+//   },
+// });
