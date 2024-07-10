157be252fdcf334e333a3e80d3bd4fd4937c9be2
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-07-10 10:17:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       "/api": {
         target: "https://cropify-deploy-server.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
