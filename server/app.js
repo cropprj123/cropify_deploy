36516e593ec97320f4f755a4376d3fe82c0884f4
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-07-24 20:18:45 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieparser = require("cookie-parser");
+const cors = require("cors");
 const cropRouter = require("./routes/cropRoutes");
 const userRouter = require("./routes/userRoutes");
 const emailRouter = require("./routes/emailRoute");
@@ -9,7 +10,6 @@ const bookingRouter = require("./routes/bookingRoutes");
 const reviewRouter = require("./routes/reviewRoute");
 const StoreRouter = require("./routes/storeRoutes");
 const addtocartRouter = require("./routes/addtocartRoute");
-const cors = require("cors");
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -35,13 +35,18 @@ app.use(
     },
     credentials: true,
     optionsSuccessStatus: 200,
-    allowedHeaders: [
-      "Content-Type",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Credentials",
-    ],
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://cropify-one.vercel.app"); // Adjust this as needed
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.options("*", cors());
 
@@ -49,6 +54,7 @@ app.options("*", cors());
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to Cropify API!");
 });
+
 app.use("/api/v1/crops", cropRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/email", emailRouter);
@@ -56,8 +62,9 @@ app.use("/api/v1/bookings", bookingRouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/store", StoreRouter);
 app.use("/api/v1/cart", addtocartRouter);
+
 app.all("*", (req, res, next) => {
-  next(new AppError(`can't find the ${req.originalUrl} url`));
+  next(new AppError(`can't find the ${req.originalUrl} url`, 404));
 });
 
 module.exports = app;
