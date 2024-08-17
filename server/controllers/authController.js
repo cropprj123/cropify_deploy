06cdc45139d43fbb624ec39d3256ec160ff1355b
cropprj123 <165889122+cropprj123@users.noreply.github.com>
2024-08-17 14:34:32 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,7 +21,7 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     sameSite: "none",
-    secure: false,
+    secure: true,
   };
   // if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
